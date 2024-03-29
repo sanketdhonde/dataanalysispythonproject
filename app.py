a8bf29abfa99ca0f3ceaab21644d92c7df4982fe
sanketdhonde <sanketdhonde222@gmail.com>
2024-03-29 16:22:29 +0530

    CHANGE

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ company = st.text_input("ENTER THE COMPANY TICKER ID")
 
 today = date.today()
 end_date = today.strftime("%Y-%m-%d")
-start_date = (today - timedelta(days=5)).strftime("%Y-%m-%d")
+start_date = (today - timedelta(days=7)).strftime("%Y-%m-%d")
 data = yf.download(company, start=start_date, end=end_date, progress=False)
 
 # Create a Streamlit app
-st.text("COMPANY BASED APPROACHED")
+st.text("SANKET PRECTION WEB PAGE")
 st.title("STOCK INDEX DATA FOR PREDICTION")
 st.write(data.head())
