cf033c67e1b068d8c9c7752d95ef9f698ed2c545
sanketdhonde <sanketdhonde222@gmail.com>
2024-03-29 16:14:03 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import yfinance as yf
+from datetime import date, timedelta
+import streamlit as st
+import yfinance
+company = st.text_input("ENTER THE COMPANY TICKER ID")
+
+today = date.today()
+end_date = today.strftime("%Y-%m-%d")
+start_date = (today - timedelta(days=5)).strftime("%Y-%m-%d")
+data = yf.download(company, start=start_date, end=end_date, progress=False)
+
+# Create a Streamlit app
+st.text("COMPANY BASED APPROACHED")
+st.title("STOCK INDEX DATA FOR PREDICTION")
+st.write(data.head())
